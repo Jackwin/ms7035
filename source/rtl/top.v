64f03c8069504c87ad95b0506f98d6cfd5a79571
Jackwin <rcwcj@126.com>
2022-04-23 21:51:22 +0800

    Sync with the CAD-MS7035 board, LED push button test done

--- source/rtl/top.v
+++ source/rtl/top.v
@@ -21,41 +21,126 @@
 
 
 module top(
-    input   clk_50m,
-    // input   rstï¼?
-    input       mipi_phy_clk_hs_p,
-    input       mipi_phy_clk_hs_n,
-    input       mipi_phy_clk_lp_p,
-    input       mipi_phy_clk_lp_n,
-    input[1:0]  mipi_phy_data_hs_p,
-    input[1:0]  mipi_phy_data_hs_n,
-    input[1:0]  mipi_phy_data_lp_p,
-    input[1:0]  mipi_phy_data_lp_n,
-  
-    output  usr_led12,
-    output  usr_led17,
-    output  usr_led22
+  input   clk_50m,
+  // input   rstï¿????
+  input       mipi_phy_clk_hs_p,
+  input       mipi_phy_clk_hs_n,
+  input       mipi_phy_clk_lp_p,
+  input       mipi_phy_clk_lp_n,
+  input[1:0]  mipi_phy_data_hs_p,
+  input[1:0]  mipi_phy_data_hs_n,
+  input[1:0]  mipi_phy_data_lp_p,
+  input[1:0]  mipi_phy_data_lp_n,
+
+  input[3:0]  slide_button,
+  input[1:0]  key,
+
+  output [3:0] usr_led
+   
 );
 
-reg [26:0]  led_cnt;
+reg [27:0]  led_cnt;
+
+reg [13:0] delay_cnt0;
+reg [13:0] delay_cnt1;
+reg [1:0] key_reg;
+reg       key0_value;
+reg       key1_value;
+reg       key0_value_r;
+reg       key1_value_r;
+
+reg       key0_value_rising;
+reg       key1_value_rising;
+reg [3:0] usr_led_r;
+
+reg[1:0]  led_mode;
 
 always @(posedge clk_50m) begin
     led_cnt <= led_cnt + 1'd1;
 end
 
-assign usr_led12 = led_cnt[26];
-assign usr_led17 = led_cnt[26];
-assign usr_led22 = led_cnt[26];
+always @(posedge clk_50m) begin
+  if (key[0]) begin
+    delay_cnt0 <= delay_cnt0 + 1'd1;
+  end else 
+    delay_cnt0 <= 'h0;
 
-  ms7035 ms7035_i(
-    .mipi_phy_clk_hs_n(mipi_phy_clk_hs_n),
-    .mipi_phy_clk_hs_p(mipi_phy_clk_hs_p),
-    .mipi_phy_clk_lp_n(mipi_phy_clk_lp_n),
-    .mipi_phy_clk_lp_p(mipi_phy_clk_lp_p),
-    .mipi_phy_data_hs_n(mipi_phy_data_hs_n),
-    .mipi_phy_data_hs_p(mipi_phy_data_hs_p),
-    .mipi_phy_data_lp_n(mipi_phy_data_lp_n),
-    .mipi_phy_data_lp_p(mipi_phy_data_lp_p)
+  if (key[0] & delay_cnt0[13]) begin
+      key0_value <= 1'b1;
+  end else begin
+      key0_value <= 1'b0;
+  end
+end
+
+always @(posedge clk_50m) begin
+  if (key[1]) begin
+    delay_cnt1 <= delay_cnt1 + 1'd1;
+  end else 
+    delay_cnt1 <= 'h0;
+
+  if (key[1] & delay_cnt1[12]) begin
+      key1_value <= 1'b1;
+  end else begin
+      key1_value <= 1'b0;
+  end
+end
+
+always @(posedge clk_50m )begin
+  key0_value_r <= key0_value;
+  key1_value_r <= key1_value;
+
+  key0_value_rising <= ~key0_value_r & key0_value;
+  key1_value_rising <= ~key1_value_r & key1_value;
+
+  if (key0_value_rising) begin
+    led_mode <= 2'd1;
+  end else if (key1_value_rising) begin
+    led_mode <= 2'd2;
+  end else begin
+    led_mode <= led_mode;
+  end
+end
+
+always @(posedge clk_50m) begin
+  case(led_mode)
+  2'd0: begin
+    usr_led_r <= {4{led_cnt[26]}};
+  end
+  2'd1, 2'd3: begin
+    usr_led_r[0] <= ~led_cnt[27] & led_cnt[26];
+    usr_led_r[1] <= led_cnt[27] & ~led_cnt[26];
+    usr_led_r[2] <= led_cnt[27] & led_cnt[26];
+    usr_led_r[3] <= ~led_cnt[27] & ~led_cnt[26];
+  end
+  2'd2: begin
+    usr_led_r[3] <= ~led_cnt[27] & led_cnt[26];
+    usr_led_r[2] <= led_cnt[27] & ~led_cnt[26];
+    usr_led_r[1] <= led_cnt[27] & led_cnt[26];
+    usr_led_r[0] <= ~led_cnt[27] & ~led_cnt[26];
+  end
+  endcase
+end
+
+assign usr_led = usr_led_r;
+
+
+ms7035 ms7035_i(
+  .mipi_phy_clk_hs_n(mipi_phy_clk_hs_n),
+  .mipi_phy_clk_hs_p(mipi_phy_clk_hs_p),
+  .mipi_phy_clk_lp_n(mipi_phy_clk_lp_n),
+  .mipi_phy_clk_lp_p(mipi_phy_clk_lp_p),
+  .mipi_phy_data_hs_n(mipi_phy_data_hs_n),
+  .mipi_phy_data_hs_p(mipi_phy_data_hs_p),
+  .mipi_phy_data_lp_n(mipi_phy_data_lp_n),
+  .mipi_phy_data_lp_p(mipi_phy_data_lp_p)
+);
+
+vio_slide_button vio_slide_button_inst(
+  .clk(clk_50m),
+  .probe_in0(slide_button[0]),
+  .probe_in1(slide_button[1]),
+  .probe_in2(slide_button[2]),
+  .probe_in3(slide_button[3])
   );
 
 endmodule
